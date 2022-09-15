1bd3d76c5e959121c301e4e466f58151760bccc8
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-15 16:21:13 +0300

    Set request sender as post object owner

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,6 +4,16 @@ from posts.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    '''
+    Read-only fields are included in the API output but should not be included
+    in the input during create or update operations. Any 'read_only' fields
+    that are incorrectly included in the serializer input will be ignored.
+    Set this to True to ensure that the field is used when serializing
+    a representation, but is not used when creating or updating an instance
+    during deserialization.
+    '''
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Post
         fields = "__all__"
--- posts/views.py
+++ posts/views.py
@@ -12,3 +12,13 @@ class PostViewSet(
 ):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        '''
+        The pre_save and post_save hooks no longer exist, but are replaced with
+        perform_create(self, serializer) and perform_update(self, serializer).
+        These methods should save the object instance by calling
+        serializer.save(), adding in any additional arguments as required.
+        They may also perform any custom pre-save or post-save behavior.
+        '''
+        serializer.save(user=self.request.user)
