2c68381b20f2003bea9873955cea9a11e1cf7356
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-15 15:40:42 +0300

    Add posts

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+from accounts.models import User
+from django.db import models
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    body = models.CharField(max_length=255, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
