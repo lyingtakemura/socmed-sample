728b48763c38f9c71475f9803f7c88f0939b98e5
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-16 11:44:34 +0300

    Add comment model

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,11 @@ class Post(models.Model):
     body = models.CharField(max_length=255, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    body = models.CharField(max_length=255, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
