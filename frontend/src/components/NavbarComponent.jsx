8875957dca6d0ba39a4ec3c7ab3e6c81f8600e54
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-18 13:27:19 +0300

    Add navbar

---
+++ frontend/src/components/NavbarComponent.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Navbar, Nav } from 'rsuite';
+
+import HomePage from "../pages/HomePage"
+import AboutPage from "../pages/AboutPage"
+
+class NavbarComponent extends React.Component {
+  render() {
+    return (
+    <Navbar>
+        <Navbar.Brand href="#">SOCMED-SAMPLE</Navbar.Brand>
+        <Nav>
+            <Nav.Item as={Link} to="/" element={<HomePage />}>
+                Home
+            </Nav.Item>
+            <Nav.Item as={Link} to="/about" element={<AboutPage />}>
+                About
+            </Nav.Item>
+        </Nav>
+    </Navbar>
+    )
+  }
+}
+
+export default NavbarComponent;
