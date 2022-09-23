fb5d208c04cf7a171e89759687cf6dda268c5e13
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-23 12:51:48 +0300

    Update navbar

--- frontend/src/components/NavbarComponent.jsx
+++ frontend/src/components/NavbarComponent.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import { React } from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux'
+
+import { logout } from '../redux/usersSlice'
 
 import { Navbar, Nav } from 'rsuite';
 
@@ -7,25 +10,32 @@ import HomePage from "../pages/HomePage"
 import AboutPage from "../pages/AboutPage"
 import LoginPage from "../pages/LoginPage"
 
-class NavbarComponent extends React.Component {
-  render() {
+
+export function NavbarComponent() {
+    const token = useSelector((state) => state.users.user.token)
+    const dispatch = useDispatch();
+
     return (
-    <Navbar>
-        <Navbar.Brand href="#">SOCMED-SAMPLE</Navbar.Brand>
-        <Nav>
-            <Nav.Item as={Link} to="/" element={<HomePage />}>
-                Home
-            </Nav.Item>
-            <Nav.Item as={Link} to="/about" element={<AboutPage />}>
-                About
-            </Nav.Item>
-            <Nav.Item as={Link} to="/login" element={<LoginPage />}>
-                Login
-            </Nav.Item>
-        </Nav>
-    </Navbar>
+        <Navbar>
+            <Navbar.Brand href="#">SOCMED-SAMPLE </Navbar.Brand>
+            <Nav>
+                <Nav.Item as={Link} to="/login" element={<LoginPage />} >
+                    Login
+                </Nav.Item>
+
+                <Nav.Item as={Link} to="/" element={<HomePage />}>
+                    Home
+                </Nav.Item>
+                <Nav.Item as={Link} to="/about" element={<AboutPage />}>
+                    About
+                </Nav.Item>
+                <Nav.Item href="#" onClick={() => dispatch(logout())}>
+                    Logout
+                </Nav.Item>
+                <Nav.Item>TOKEN:{token}</Nav.Item>
+            </Nav>
+        </Navbar>
     )
-  }
 }
 
 export default NavbarComponent;
