92e867b323d04fb1ce2c0680658e99dd2b1ece6f
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-27 21:19:57 +0300

    Update ws consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,6 @@
 import json
-from random import randint
-from time import sleep
+# from random import randint
+# from time import sleep
 
 from channels.generic.websocket import WebsocketConsumer
 
@@ -9,17 +9,18 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
-        for i in range(20):
-            self.send(json.dumps({'message': randint(1, 300)}))
-            sleep(2)
+        # for i in range(3):
+        #     self.send(json.dumps({'message': randint(1, 300)}))
+        #     sleep(2)
 
     def disconnect(self, close_code):
         pass
 
-    def receive(self, text_data):
+    def receive(self, text_data=None):
         text_data_json = json.loads(text_data)
+        print(json.loads(text_data))
         message = text_data_json['message']
 
-        self.send(text_data=json.dumps({
+        self.send(json.dumps({
             'message': message
         }))
