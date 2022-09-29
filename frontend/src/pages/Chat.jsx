2c0ae85c4f205238c978b0735d1f0fa58a4930ae
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-29 15:07:01 +0300

    Use rsuit components for chat page

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-
+import { FlexboxGrid, Panel, Input, InputGroup, Form, } from 'rsuite';
+import SendIcon from '@rsuite/icons/Send';
 
 const Feed = (props) => {
-  // const [posts, setPosts] = useState()
   const [input, setInput] = useState("")
   const ws = props.ws
 
@@ -11,21 +11,48 @@ const Feed = (props) => {
   }, []) // without second param useEffect will stuck in update loop  
 
 
-  const handleSendMessage = e => {
-    e.preventDefault()
+  const sendMessage = () => {
     console.log(input)
     ws.send(JSON.stringify({
       'message': input
     }))
+    setInput("")
   }
 
   return (
   <> 
-    <h1>CHAT</h1>
-    <form onSubmit={event => handleSendMessage(event)}>
-      <input placeholder='type message' value={input} onChange={event => setInput(event.target.value)} />
-      <input type="submit" value="send" />
-    </form>
+    <FlexboxGrid justify="center" style={{marginTop: "1rem"}}>
+      <FlexboxGrid.Item colspan={6}>
+        <Panel header="{CHATS_LIST}" bordered style={{ height: "80vh"}}>
+          <Panel bordered>
+            USER
+          </Panel>
+        </Panel>  
+      </FlexboxGrid.Item>
+      &nbsp;
+      <FlexboxGrid.Item colspan={12}>
+        <Panel header="{CURRENT_CHAT_NAME}" bordered style={{ height: "80vh"}}>
+          <Panel bordered>
+            MESSAGE_BODY
+          </Panel>
+
+          {/* <form onSubmit={event => sendMessage(event)}>
+            <input placeholder='type message' value={input} onChange={event => setInput(event.target.value)} />
+            <input type="submit" value="send" />
+          </form> */}
+
+          <Form onSubmit={sendMessage} style={{marginTop: "1rem"}}>
+            <InputGroup size="lg">
+              <Input value={input} onChange={event => setInput(event)} />
+              <InputGroup.Button type="submit">
+                <SendIcon  />
+              </InputGroup.Button>
+            </InputGroup>
+          </Form>
+
+        </Panel>  
+      </FlexboxGrid.Item>
+    </FlexboxGrid>
   </>
   );
 }
