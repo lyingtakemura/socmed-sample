0c48e7dbb16856a098241de32e4c5a4eeedc9b7b
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-09-30 13:56:41 +0300

    Load contacts list in useEffect

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { FlexboxGrid, Panel, Input, InputGroup, Form, } from 'rsuite';
 import SendIcon from '@rsuite/icons/Send';
+import axios from "axios"
+import { useSelector } from 'react-redux'
 
 const Feed = (props) => {
   const [input, setInput] = useState("")
+  const [messages, setMessages] = useState("")
+  const [contacts, setContacts] = useState("")
+  let currentUser = useSelector((state) => state.users.currentUser)
   const ws = props.ws
 
   useEffect(() => {
-
+    axios.get("http://127.0.0.1:8000/contacts/", { 
+      headers: {
+        'Authorization': 'Token ' + currentUser.token
+      }
+    })
+    .then((response) => {
+      setContacts(response.data)
+      console.log(response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
   }, []) // without second param useEffect will stuck in update loop  
 
 
@@ -21,19 +36,22 @@ const Feed = (props) => {
 
   return (
   <> 
-    <FlexboxGrid justify="center" style={{marginTop: "1rem"}}>
+    <FlexboxGrid justify="center" style={{marginTop: "0.5rem"}}>
       <FlexboxGrid.Item colspan={6}>
         <Panel header="{CHATS_LIST}" bordered style={{ height: "80vh"}}>
-          <Panel bordered>
-            USER
-          </Panel>
+          { contacts && contacts.map((contact) => 
+              <Panel bordered style={{marginBottom: "0.5rem"}} key={contact.id} >
+                {contact.username}
+              </Panel>
+            )
+          }
         </Panel>  
       </FlexboxGrid.Item>
       &nbsp;
       <FlexboxGrid.Item colspan={12}>
         <Panel header="{CURRENT_CHAT_NAME}" bordered style={{ height: "80vh"}}>
           <Panel bordered>
-            MESSAGE_BODY
+            {messages}
           </Panel>
 
           {/* <form onSubmit={event => sendMessage(event)}>
