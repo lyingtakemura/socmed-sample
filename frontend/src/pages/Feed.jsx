d021276779294f5f482c3318e75ddba366b1439a
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-10-06 17:23:12 +0300

    Add posts input form

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Panel, PanelGroup } from "rsuite";
+import { useSelector } from "react-redux";
+import {
+    FlexboxGrid,
+    Panel,
+    Input,
+    InputGroup,
+    Form,
+    PanelGroup,
+} from "rsuite";
+import SendIcon from "@rsuite/icons/Send";
 
 const Feed = () => {
-    const [posts, setPosts] = useState();
+    let currentUser = useSelector((state) => state.users.currentUser);
+    const [posts, setPosts] = useState("");
+    const [input, setInput] = useState("");
 
     useEffect(() => {
         axios
@@ -15,25 +26,74 @@ const Feed = () => {
             .catch((error) => {
                 console.log(error);
             });
-    }, []); // without second param useEffect will stuck in update loop
+    }, [posts]);
+
+    const sendPost = () => {
+        axios
+            .post(
+                "http://127.0.0.1:8000/posts/",
+                {
+                    body: input,
+                },
+                {
+                    headers: {
+                        Authorization: "Token " + currentUser.token,
+                    },
+                }
+            )
+            .then((response) => {
+                console.log(response.data);
+                let obj = response.data;
+                let posts_temp = [...posts, obj];
+                setPosts(posts_temp);
+                // ws.send(
+                //     JSON.stringify({
+                //         message: response.data,
+                //     })
+                // );
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        setInput("");
+    };
 
     return (
         <>
-            <PanelGroup>
-                {posts &&
-                    posts.map(
-                        (
-                            post //check if posts array have been loaded from axios request to state before render
-                        ) => (
-                            <Panel
-                                header={`USER: ${post.user} POSTED AT ${post.created_at}`}
-                                key={post.id}
-                            >
-                                {post.body}
-                            </Panel>
-                        )
-                    )}
-            </PanelGroup>
+            <FlexboxGrid justify="center" style={{ marginTop: "0.5rem" }}>
+                <FlexboxGrid.Item colspan={12}>
+                    <Panel bordered style={{ marginTop: "0.5rem" }}>
+                        <Form onSubmit={sendPost}>
+                            <InputGroup size="lg">
+                                <Input
+                                    value={input}
+                                    onChange={(event) => setInput(event)}
+                                    required
+                                />
+                                <InputGroup.Button type="submit">
+                                    <SendIcon />
+                                </InputGroup.Button>
+                            </InputGroup>
+                        </Form>
+                    </Panel>
+
+                    <PanelGroup>
+                        {posts &&
+                            posts.map(
+                                (
+                                    post //check if posts array have been loaded from axios request to state before render
+                                ) => (
+                                    <Panel
+                                        header={`USER: ${post.user} POSTED AT ${post.created_at}`}
+                                        key={post.id}
+                                    >
+                                        {post.body}
+                                    </Panel>
+                                )
+                            )}
+                    </PanelGroup>
+                </FlexboxGrid.Item>
+            </FlexboxGrid>
         </>
     );
 };
