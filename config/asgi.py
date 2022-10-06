295847e27c9b7e36dc694e990bbe4e27f4ea99b6
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-10-06 15:34:24 +0300

    Add TokenAuthMiddleware

--- config/asgi.py
+++ config/asgi.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
+# from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.routing import ws_urlpatterns
 from django.core.asgi import get_asgi_application
+
+from config.middleware import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
@@ -22,5 +24,5 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
-  'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
+  'websocket': TokenAuthMiddleware(URLRouter(ws_urlpatterns))
 })
