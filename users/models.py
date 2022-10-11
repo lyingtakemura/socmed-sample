f6a4fa8abc809d1e82043b5d2da927f106a1affe
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-10-11 19:59:45 +0300

    Add many-to-many followers field to custom user model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    '''
+    When Django processes this model, it identifies that it has a
+    ManyToManyField on itself, and as a result, it doesn't add a followers
+    attribute to the User class. Instead, the ManyToManyField is assumed to be
+    symmetrical - that is, if I am your follower, then you are my follower.
+
+    If you do not want symmetry in many-to-many relationships with self, set
+    symmetrical to False. This will force Django to add the descriptor for the
+    reverse relationship, allowing ManyToManyField relationships
+    to be non-symmetrical.
+    '''
+    following = models.ManyToManyField('self', related_name='following_set', symmetrical=False)
+    followers = models.ManyToManyField('self', related_name='followers_set', symmetrical=False)
