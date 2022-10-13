e57151bc5bf9988f6d4a4b5de79af2ddf95114ae
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-10-13 20:00:03 +0300

    Remove chat app

--- messenger/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from messenger.models import Message, Thread
-
-admin.site.register(Message)
-admin.site.register(Thread)
--- messenger/routing.py
+++
@@ -1,8 +0,0 @@
-# chat/routing.py
-from django.urls import path
-
-from messenger.consumers import ChatConsumer
-
-ws_urlpatterns = [
-    path('ws/test/', ChatConsumer.as_asgi())
-]
