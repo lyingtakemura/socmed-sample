0002bd7b766351d33897747cd521cdf6d56b805b
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-10-19 16:55:28 +0300

    Update serializers tests

--- factories.py
+++ factories.py
@@ -1,5 +1,6 @@
 import factory
 
+from messenger.models import Message, Thread
 from posts.models import Post
 from users.models import User
 
@@ -8,6 +9,8 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
+    email = factory.Faker("email")
+    username = "testuser"
     is_superuser = False
     is_staff = False
 
@@ -22,5 +25,33 @@ class PostFactory(factory.django.DjangoModelFactory):
         model = Post
 
     user = factory.SubFactory(UserFactory)
+    body = factory.Faker("sentence")
+    created_at = factory.Faker("date_time")
+
+
+class ThreadFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Thread
+
+    type = "personal"
+
+    @factory.post_generation
+    def groups(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of users were passed in, use them
+            for users in extracted:
+                self.users.add(users)
+
+
+class MessageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Message
+
+    thread = factory.SubFactory(ThreadFactory)
+    sender = factory.SubFactory(UserFactory)
     body = factory.Faker("sentence")
     created_at = factory.Faker("date_time")
--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -1,5 +1,8 @@
+import factory
 import pytest
-from factories import PostFactory, UserFactory
+from factories import MessageFactory, PostFactory, ThreadFactory, UserFactory
+from messenger.models import Message, Thread
+from messenger.serializers import MessageSerializer, ThreadSerializer
 from posts.serializers import PostSerializer
 
 
@@ -17,3 +20,41 @@ class TestPostSerializer:
         serializer = PostSerializer(post)
 
         assert serializer.data
+
+    def test_serialized_data(self, mocker):
+        valid_serialized_data = factory.build(dict, FACTORY_CLASS=PostFactory)
+
+        serializer = PostSerializer(data=valid_serialized_data)
+
+        assert serializer.is_valid()
+        assert serializer.errors == {}
+
+
+class TestMessageSerializer:
+    class Meta:
+        model = Message
+
+    @pytest.mark.unit
+    def test_serialize_model(self):
+        message = MessageFactory.build()
+        serializer = MessageSerializer(message)
+
+        assert serializer.data
+
+    def test_serialized_data(self, mocker):
+        valid_serialized_data = factory.build(dict, FACTORY_CLASS=MessageFactory)
+
+        serializer = MessageSerializer(data=valid_serialized_data)
+
+        assert serializer.is_valid()
+        assert serializer.errors == {}
+
+
+# class TestThreadSerializer: # RecursionError
+#     class Meta:
+#         model = Thread
+
+#     def test_serialize_model(self):
+#         thread = ThreadFactory.build()
+#         serializer = ThreadSerializer(thread)
+#         assert serializer.data
