0571fa007d7ac120d8ee3b724197677b72a1f73e
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2022-11-08 20:54:46 +0200

    Use AWS S3 for static and uploads

--- config/settings.py
+++ config/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "djoser",
     "channels",
+    "storages",
     "users",
     "posts",
     "messenger",
@@ -132,11 +133,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -162,15 +158,29 @@ CHANNEL_LAYERS = {
     },
 }
 
-
-# Actual directory user files go to
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "mediafiles")
-
-# URL used to access the media
-MEDIA_URL = "/media/"
-
 DJOSER = {
     "SERIALIZERS": {
         "current_user": "users.serializers.UserSerializer",
     }
 }
+
+
+if os.getenv("USE_S3"):
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
+
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_FILE_OVERWRITE = False
+
+    AWS_STATIC_LOCATION = "static"
+    AWS_PUBLIC_MEDIA_LOCATION = "media"
+    STATIC_URL = "https://{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+    MEDIA_URL = "https://{}/{}/".format(AWS_S3_CUSTOM_DOMAIN, AWS_PUBLIC_MEDIA_LOCATION)
+else:
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+    STATIC_URL = "/static/"
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
