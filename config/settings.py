506f3d7533ccf43a74a4b0199126bba7da4ac872
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2023-05-31 23:23:03 +0300

    Use redis for cache

--- config/settings.py
+++ config/settings.py
@@ -192,3 +192,10 @@ else:
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
