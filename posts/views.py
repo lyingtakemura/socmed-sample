a6f98e0c1a7aaf0eb213893811aadd8bd2daecfb
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2023-06-07 20:43:53 +0300

    Improve posts caching

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,11 @@
-# from django.db.models import Q
+from django.core.cache import cache
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, viewsets
-
-# from rest_framework.response import Response
+from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle
 
 from posts.models import Post
 from posts.serializers import PostSerializer
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
 
 
 class PostViewSet(
@@ -23,22 +20,25 @@ class PostViewSet(
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["user"]
 
-    @method_decorator(cache_page(20))
-    def dispatch(self, *args, **kwargs):
-        return super(PostViewSet, self).dispatch(*args, **kwargs)
+    def list(self, request, *args, **kwargs):
+        if not cache.get("query_params") == request.query_params:
+            cache.set("query_params", request.query_params)
+            cache.delete("posts")  # delete cached posts on url filtering params change
 
-    # def list(self, request):
-    #     """
-    #     To serialize a queryset or list of objects instead of a single object
-    #     instance, you should set many=True flag when instantiating serializer.
+        if "posts" in cache:
+            print("CACHE_PING")
+            return Response(cache.get("posts"))
+        else:
+            queryset = self.filter_queryset(self.get_queryset())
 
-    #     Get only posts of users which current user is following and self
-    #     """
-    #     queryset = Post.objects.filter(
-    #         Q(user__in=self.request.user.following.all()) | Q(user=self.request.user)
-    #     ).order_by("-created_at")
-    #     serializer = PostSerializer(queryset, many=True)
-    #     return Response(serializer.data)
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(queryset, many=True)
+            cache.set("posts", serializer.data, timeout=3600)
+            return Response(serializer.data)
 
     def perform_create(self, serializer):
         """
@@ -48,4 +48,6 @@ class PostViewSet(
         serializer.save(), adding in any additional arguments as required.
         They may also perform any custom pre-save or post-save behavior.
         """
+        cache.delete("posts")
+        print("CACHE_DROP")
         serializer.save(user=self.request.user)
