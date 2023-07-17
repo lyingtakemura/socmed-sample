bc47141e3b1d39b9ada606ee370f28467207f58b
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2023-07-17 19:35:30 +0300

    Add post model tests

---
+++ config/fixtures/user.py
@@ -0,0 +1,15 @@
+import pytest
+from users.models import User
+
+data_user = {
+    "username": "test_user",
+    "email": "test@gmail.com",
+    "first_name": "Test",
+    "last_name": "User",
+    "password": "test_password",
+}
+
+
+@pytest.fixture
+def user(db) -> User:
+    return User.objects.create_user(**data_user)
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import pytest
+from config.fixtures.user import user
+from posts.models import Post
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_create_post(user):
+    post = Post.objects.create(user=user, body="Test Post Body")
+    assert post.body == "Test Post Body"
+    assert post.user.id == user.id
