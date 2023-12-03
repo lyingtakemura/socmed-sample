75b17b408d6e4581f310867f8df0192d973b7fef
lyingtakemura <106205052+lyingtakemura@users.noreply.github.com>
2023-12-03 11:33:58 +0200

    Update docker image build

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -14,8 +14,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-# get_asgi_application should me imported before middleware
-# for "daphne config.asgi:application" to run
 django_asgi_app = get_asgi_application()
 
 from config.middleware import TokenAuthMiddleware
